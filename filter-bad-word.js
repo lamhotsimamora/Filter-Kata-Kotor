6c3e98c4c41d11618693fc352fc70b6d190672ff
Lamhot Simamora <lamhotsimamora36@gmail.com>
2017-11-27 11:00:03 +0700

    Update filter-bad-word.js

--- filter-bad-word.js
+++ filter-bad-word.js
@@ -1,35 +1,6 @@
 // Source https://raw.githubusercontent.com/lamhotsimamora/Filter-Kata-Kotor/master/filter-bad-word.js
 // License https://raw.githubusercontent.com/lamhotsimamora/Filter-Kata-Kotor/master/LICENSE
 const _badWord = [
-	'Anjing','Babi','Monyet','Kunyuk','Bajingan','Asu','Bangsat'
-	,'Kampret','Kontol','Memek','Ngentot','Pentil','Perek','Pepek'
-	,'Pecun','Bencong','Banci','Jablay','Maho','Bego','Goblok','Idiot'
-	,'Geblek','Gila','Sinting','Tolol','Sarap','Setan','Keparat','Bejad'
-	,'Gembel','Brengsek','Tai','Anjrit','Bangsat','Fuck','Tetek','Ngulum'
-	,'Jembut','Totong','Kolop'
+	'Anjing','Babi','Monyet','Kunyuk','Bajingan','Asu','Bangsat','Kampret','Kontol','Memek','Ngentot','Pentil','Perek','Pepek','Pecun','Bencong','Banci','Jablay','Maho','Bego','Goblok','Idiot','Geblek','Gila','Sinting','Tolol','Sarap','Setan','Keparat','Bejad','Gembel','Brengsek','Tai','Anjrit','Bangsat','Fuck','Tetek','Ngulum','Jembut','Totong','Kolop'
 ];
-
-String.prototype._replaceAllString = function(s, r) {
-    return this.split(s).join(r);
-};
-
-function _filterBadWord(str,txt,dt){
-	if (str)
-	{
-		var str = str.toLowerCase();
-		txt = txt ? txt : "***";
-		dt = dt ? dt : _badWord;
-		for (var i = 0; i < dt.length; i++) 
-		{
-			 var kk = dt[i].toLowerCase();
-			 var ii = str.search(kk);
-			 if ( ii != -1)
-			 {
-			 	 str = str._replaceAllString(kk,txt);
-			 }	
-		}
-		return str;
-	}else{
-		return undefined;
-	}
-}
+String.prototype._replaceAllString = function(s, r) {return this.split(s).join(r); }; function _filterBadWord(str,txt,dt){if (str) {var str = str.toLowerCase(); txt = txt ? txt : "***"; dt = dt ? dt : _badWord; for (var i = 0; i < dt.length; i++) {var kk = dt[i].toLowerCase(); var ii = str.search(kk); if ( ii != -1) {str = str._replaceAllString(kk,txt); } } return str; }else{return undefined; } }
